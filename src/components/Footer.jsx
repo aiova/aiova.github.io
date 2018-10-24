79a5525e83106110ba4379e37e79e7788f172762
oleg <oleg@nexla.com>
2018-10-24 17:41:47 +0300

    Removed phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => (
     <div className="Footer">
         <div className="Content Footer__content">
             <a href="mailto:ai.aiova.tech@gmail.com" className="Footer__email">ai.aiova.tech@gmail.com</a>
-            <a href="tel:+79999999999" className="Footer__phone">+7 999 999 99 99</a>
         </div>
     </div>
 )
